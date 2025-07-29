932f5405b6b1a1a7bedb4fbffeae22270bc6a561
Anthony Templeton <twantempleton11@gmail.com>
2025-07-29 01:41:07 -0400

    fix display bug on bufferline

--- dot_config/nvim/plugin/bufferline.lua
+++ dot_config/nvim/plugin/bufferline.lua
@@ -2,4 +2,13 @@ vim.pack.add({
   { src = "https://github.com/akinsho/bufferline.nvim" },
 })
 vim.opt.termguicolors = true
-require("bufferline").setup({})
+local bufferline = require('bufferline')
+bufferline.setup({
+  options = {
+    diagnostics = "nvim_lsp",
+    numbers = "none",
+    separator_style = "thin",
+    always_show_bufferline = false
+  }
+
+})
