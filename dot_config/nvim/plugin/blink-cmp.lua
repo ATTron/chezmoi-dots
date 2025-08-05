3a2f99ec717d6a9162eb2216a5d13b96863b6d5c
Anthony Templeton <twantempleton11@gmail.com>
2025-08-05 04:06:22 +0000

    Update .config/nvim/plugin/blink-cmp.lua

--- dot_config/nvim/plugin/blink-cmp.lua
+++ dot_config/nvim/plugin/blink-cmp.lua
@@ -3,9 +3,17 @@ vim.pack.add({
     src = "https://github.com/saghen/blink.cmp",
     version = "v1.3.1",
   },
+  { src = "https://github.com/L3MON4D3/LuaSnip", }
 })
 
 local utils = require("utils")
+
+local success = utils.ensure_plugin_built(
+  'https://github.com/L3MON4D3/LuaSnip',
+  'LuaSnip',
+  'make install_jsregexp',
+  'lua/luasnip-jsregexp.lua'
+)
 
 local success = utils.ensure_plugin_built(
   'https://github.com/saghen/blink.cmp.git',
@@ -13,6 +21,33 @@ local success = utils.ensure_plugin_built(
   'cargo build --release',
   'target/release/libblink_cmp_fuzzy.so'
 )
+
+function in_treesitter_capture(capture)
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  if vim.api.nvim_get_mode().mode == 'i' then
+    col = col - 1
+  end
+
+  local buf = vim.api.nvim_get_current_buf()
+  local get_captures_at_pos = require('vim.treesitter').get_captures_at_pos
+
+  local captures_at_cursor = vim.tbl_map(function(x)
+    return x.capture
+  end, get_captures_at_pos(buf, row - 1, col))
+
+  if vim.tbl_isempty(captures_at_cursor) then
+    return false
+  elseif type(capture) == 'string' and vim.tbl_contains(captures_at_cursor, capture) then
+    return true
+  elseif type(capture) == 'table' then
+    for _, v in ipairs(capture) do
+      if vim.tbl_contains(captures_at_cursor, v) then
+        return true
+      end
+    end
+  end
+  return false
+end
 
 
 if success then
@@ -29,7 +64,7 @@ if success then
     },
 
     appearance = {
-      use_nvim_cmp_as_default = true,
+      -- use_nvim_cmp_as_default = true,
       nerd_font_variant = 'mono'
     },
 
@@ -40,6 +75,7 @@ if success then
     completion = {
       trigger = {
         show_on_blocked_trigger_characters = { ' ', '\n', '\t' },
+        show_on_x_blocked_trigger_characters = { "'", '"', '(' },
         show_in_snippet = false,
       },
       accept = {
@@ -55,12 +91,15 @@ if success then
         }
       },
       menu = {
-        auto_show = true,
+        auto_show = function()
+          return not require("luasnip").expand_or_jumpable() and not in_treesitter_capture("string")
+        end,
+        -- auto_show = true,
         draw = {
           columns = { { "kind_icon", "label", "label_description", gap = 1 }, { "kind" } },
         }
       },
-      ghost_text = { enabled = true },
+      ghost_text = { enabled = false },
     },
 
 
