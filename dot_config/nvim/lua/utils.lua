f032faab339377642ff2eecdc6f6cac3db77fa11
Anthony Templeton <twantempleton11@gmail.com>
2025-08-13 15:40:17 -0400

    fix for macos

--- dot_config/nvim/lua/utils.lua
+++ dot_config/nvim/lua/utils.lua
@@ -1,15 +1,22 @@
 local M = {}
-
 function M.ensure_plugin_built(repo_url, plugin_name, build_cmd, build_check_file)
   local pack_path = vim.fn.stdpath('data') .. '/site/pack/core/opt'
   local plugin_path = pack_path .. '/' .. plugin_name
   vim.fn.mkdir(pack_path, 'p')
+
   if vim.fn.isdirectory(plugin_path) == 0 then
     print('Installing ' .. plugin_name .. '...')
     vim.fn.system('git clone ' .. repo_url .. ' ' .. plugin_path)
   end
-  local build_artifact = plugin_path .. '/' .. build_check_file
-  if vim.fn.filereadable(build_artifact) == 0 then
+
+  local base_path = plugin_path .. '/' .. build_check_file
+  local so_file = base_path .. '.so'
+  local dylib_file = base_path .. '.dylib'
+  local lua_file = base_path .. '.lua'
+
+  local artifact_exists = vim.fn.filereadable(so_file) == 1 or vim.fn.filereadable(dylib_file) == 1 or vim.fn.filereadable(lua_file) == 1
+
+  if not artifact_exists then
     print('Building ' .. plugin_name .. '...')
     local result = vim.fn.system('cd ' .. plugin_path .. ' && ' .. build_cmd)
     if vim.v.shell_error == 0 then
@@ -19,7 +26,9 @@ function M.ensure_plugin_built(repo_url, plugin_name, build_cmd, build_check_fil
       return false
     end
   end
+
   return true
 end
 
 return M
+
