2ac76e6665a615715d9994c2e75aff955156bb92
Anthony Templeton <twantempleton11@gmail.com>
2025-09-05 00:27:45 -0400

    Update .config/nvim/plugin/pairs.lua

--- dot_config/nvim/plugin/pairs.lua
+++ dot_config/nvim/plugin/pairs.lua
@@ -1,5 +1,7 @@
 vim.pack.add({
   { src = "https://github.com/echasnovski/mini.pairs" },
+  { src = "https://github.com/nvim-mini/mini.surround" },
 })
 
 require("mini.pairs").setup({})
+require("mini.surround").setup({})
