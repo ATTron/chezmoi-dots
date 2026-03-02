a0ca6c80d30f313609efcce79349aed8239b3785
Anthony Templeton <twantempleton11@gmail.com>
2026-03-01 22:30:31 -0500

    Update .config/nvim/plugin/blink-cmp.lua Update .config/nvim/plugin/fff.lua

--- dot_config/nvim/plugin/blink-cmp.lua
+++ dot_config/nvim/plugin/blink-cmp.lua
@@ -1,52 +1,23 @@
 vim.pack.add({
-  {
-    src = "https://github.com/saghen/blink.cmp",
-  },
+  { src = "https://github.com/saghen/blink.cmp" },
   { src = "https://github.com/L3MON4D3/LuaSnip" },
 })
 
 local utils = require("utils")
 
-local success = utils.ensure_plugin_built(
+utils.ensure_plugin_built(
   "https://github.com/L3MON4D3/LuaSnip",
   "LuaSnip",
   "make install_jsregexp",
   "lua/luasnip-jsregexp"
 )
 
-success = utils.ensure_plugin_built(
+local success = utils.ensure_plugin_built(
   "https://github.com/saghen/blink.cmp",
   "blink.cmp",
   "cargo build --release",
   "target/release/libblink_cmp_fuzzy"
 )
-
-local function in_treesitter_capture(capture)
-  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-  if vim.api.nvim_get_mode().mode == "i" then
-    col = col - 1
-  end
-
-  local buf = vim.api.nvim_get_current_buf()
-  local get_captures_at_pos = require("vim.treesitter").get_captures_at_pos
-
-  local captures_at_cursor = vim.tbl_map(function(x)
-    return x.capture
-  end, get_captures_at_pos(buf, row - 1, col))
-
-  if vim.tbl_isempty(captures_at_cursor) then
-    return false
-  elseif type(capture) == "string" and vim.tbl_contains(captures_at_cursor, capture) then
-    return true
-  elseif type(capture) == "table" then
-    for _, v in ipairs(capture) do
-      if vim.tbl_contains(captures_at_cursor, v) then
-        return true
-      end
-    end
-  end
-  return false
-end
 
 if success then
   require("blink.cmp").setup({
@@ -54,20 +25,10 @@ if success then
       preset = "default",
       ["<Tab>"] = { "select_next", "fallback" },
       ["<S-Tab>"] = { "select_prev", "fallback" },
-      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-      ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
-      ["<C-e>"] = { "hide", "fallback" },
-      ["<CR>"] = { "accept", "fallback" },
     },
 
     appearance = {
-      -- use_nvim_cmp_as_default = true,
       nerd_font_variant = "mono",
-    },
-
-    sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
     },
 
     completion = {
@@ -76,51 +37,24 @@ if success then
         show_on_x_blocked_trigger_characters = { "'", '"', "(" },
         show_in_snippet = false,
       },
-      accept = {
-        auto_brackets = {
-          enabled = true,
-        },
-      },
       documentation = {
         auto_show = true,
         auto_show_delay_ms = 200,
-        window = {
-          border = "rounded",
-        },
+        window = { border = "rounded" },
       },
       menu = {
         auto_show = function()
-          return not require("luasnip").expand_or_jumpable() and not in_treesitter_capture({ "comment", "string" })
+          return not require("luasnip").expand_or_jumpable()
         end,
-        -- auto_show = true,
         draw = {
           columns = { { "kind_icon", "label", "label_description", gap = 1 }, { "kind" } },
         },
       },
-      ghost_text = { enabled = false },
     },
 
     fuzzy = {
       implementation = "prefer_rust_with_warning",
-      sorts = {
-        "score",
-        "sort_text",
-        "label",
-      },
-    },
-    snippets = {
-      expand = function(snippet)
-        vim.snippet.expand(snippet)
-      end,
-      active = function(filter)
-        return vim.snippet.active(filter)
-      end,
-      jump = function(direction)
-        vim.snippet.jump(direction)
-      end,
+      sorts = { "score", "sort_text", "label" },
     },
   })
-
-  local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
 end
--- dot_config/nvim/plugin/fff.lua
+++ dot_config/nvim/plugin/fff.lua
@@ -1,7 +1,5 @@
 vim.pack.add({
-  {
-    src = "https://github.com/dmtrKovalenko/fff.nvim",
-  },
+  { src = "https://github.com/dmtrKovalenko/fff.nvim" },
 })
 
 vim.api.nvim_create_autocmd("PackChanged", {
@@ -13,43 +11,18 @@ vim.api.nvim_create_autocmd("PackChanged", {
 })
 
 vim.g.fff = {
-  lazy_sync = true,
-  debug = {
-    enabled = true,
-    show_scores = true,
-  },
+  debug = { enabled = true, show_scores = true },
 }
 
 require("fff").setup({
   prompt = "🦆 ",
   title = "Whaddya Buyin ?",
-  frecency = {
-    enabled = true,                                   -- Enable frecency tracking
-    db_path = vim.fn.stdpath("cache") .. "/fff_nvim", -- Database location
-  },
   layout = {
-    prompt_position = "top",    -- Position of prompt ('top' or 'bottom')
-    preview_position = "right", -- Position of preview ('right' or 'left')
-    preview_size = 0.5,
-    height = 0.8,               -- Window height
-    width = 0.8,                -- Window width
-  },
-  preview = {
-    max_lines = 200,
-    max_size = 5 * 1024 * 1024,
-    binary_file_threshold = 0,
+    prompt_position = "top",
   },
   keymaps = {
-    close = "<Esc>",
-    select = "<CR>",
-    select_split = "<C-s>",
-    select_vsplit = "<C-v>",
-    select_tab = "<C-t>",
     move_up = { "<S-Tab>", "<C-n>" },
-    move_down = { "<Tab>", "<C-p>" }, -- Multiple bindings supported
-    preview_scroll_up = "<C-u>",
-    preview_scroll_down = "<C-d>",
-    toggle_debug = "<F2>", -- Toggle debug scores display
+    move_down = { "<Tab>", "<C-p>" },
   },
 })
 
