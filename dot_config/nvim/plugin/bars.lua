a4a56fdbf21101dd6f62c6502f8ac58a82c12e7c
Anthony Templeton <twantempleton11@gmail.com>
2026-03-01 22:22:03 -0500

    Add .config/nvim/plugin/bars.lua

---
+++ dot_config/nvim/plugin/bars.lua
@@ -0,0 +1,42 @@
+vim.pack.add({
+  { src = "https://github.com/akinsho/bufferline.nvim" },
+  { src = "https://github.com/nvim-lualine/lualine.nvim" },
+})
+
+local bufferline = require("bufferline")
+bufferline.setup({
+  options = {
+    diagnostics = "nvim_lsp",
+    separator_style = "thin",
+    always_show_bufferline = false,
+    custom_filter = function(buf_number)
+      -- Filter out terminal buffers
+      if vim.bo[buf_number].buftype == "terminal" then
+        return false
+      end
+      return true
+    end,
+  },
+})
+
+require("lualine").setup({
+  options = {
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
+    theme = "auto",
+  },
+  sections = {
+    lualine_a = {
+      {
+        "mode",
+        fmt = function(str)
+          return str:sub(1, 1)
+        end,
+      },
+    },
+    lualine_c = { "filename" },
+    lualine_x = { "filetype" },
+    lualine_y = { "progress" },
+    lualine_z = { "location" },
+  },
+})
