5a7ed96118f31fe68c4239deb071e46094300be0
Anthony Templeton <twantempleton11@gmail.com>
2026-03-01 22:38:15 -0500

    Add .config/nvim/lua/.keep Update .config/nvim/plugin/blink-cmp.lua

--- dot_config/nvim/plugin/blink-cmp.lua
+++ dot_config/nvim/plugin/blink-cmp.lua
@@ -1,60 +1,42 @@
 vim.pack.add({
-  { src = "https://github.com/saghen/blink.cmp" },
+  { src = "https://github.com/saghen/blink.cmp", version = "v1.*" },
   { src = "https://github.com/L3MON4D3/LuaSnip" },
 })
 
-local utils = require("utils")
+require("blink.cmp").setup({
+  keymap = {
+    preset = "default",
+    ["<Tab>"] = { "select_next", "fallback" },
+    ["<S-Tab>"] = { "select_prev", "fallback" },
+  },
 
-utils.ensure_plugin_built(
-  "https://github.com/L3MON4D3/LuaSnip",
-  "LuaSnip",
-  "make install_jsregexp",
-  "lua/luasnip-jsregexp"
-)
+  appearance = {
+    nerd_font_variant = "mono",
+  },
 
-local success = utils.ensure_plugin_built(
-  "https://github.com/saghen/blink.cmp",
-  "blink.cmp",
-  "cargo build --release",
-  "target/release/libblink_cmp_fuzzy"
-)
-
-if success then
-  require("blink.cmp").setup({
-    keymap = {
-      preset = "default",
-      ["<Tab>"] = { "select_next", "fallback" },
-      ["<S-Tab>"] = { "select_prev", "fallback" },
+  completion = {
+    trigger = {
+      show_on_blocked_trigger_characters = { " ", "\n", "\t" },
+      show_on_x_blocked_trigger_characters = { "'", '"', "(" },
+      show_in_snippet = false,
     },
-
-    appearance = {
-      nerd_font_variant = "mono",
+    documentation = {
+      auto_show = true,
+      auto_show_delay_ms = 200,
+      window = { border = "rounded" },
     },
-
-    completion = {
-      trigger = {
-        show_on_blocked_trigger_characters = { " ", "\n", "\t" },
-        show_on_x_blocked_trigger_characters = { "'", '"', "(" },
-        show_in_snippet = false,
-      },
-      documentation = {
-        auto_show = true,
-        auto_show_delay_ms = 200,
-        window = { border = "rounded" },
-      },
-      menu = {
-        auto_show = function()
-          return not require("luasnip").expand_or_jumpable()
-        end,
-        draw = {
-          columns = { { "kind_icon", "label", "label_description", gap = 1 }, { "kind" } },
-        },
+    menu = {
+      auto_show = function()
+        return not require("luasnip").expand_or_jumpable()
+      end,
+      draw = {
+        columns = { { "kind_icon", "label", "label_description", gap = 1 }, { "kind" } },
       },
     },
+  },
 
-    fuzzy = {
-      implementation = "prefer_rust_with_warning",
-      sorts = { "score", "sort_text", "label" },
-    },
-  })
-end
+  fuzzy = {
+    implementation = "prefer_rust_with_warning",
+    sorts = { "score", "sort_text", "label" },
+  },
+})
