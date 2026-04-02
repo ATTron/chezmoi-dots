89d1523b8e0688e97f42119cac1fc39e8f957a24
Anthony Templeton <twantempleton11@gmail.com>
2026-04-02 00:47:22 -0400

    Remove .config/nvim/after/ftplugin/netrw.lua

--- dot_config/nvim/after/ftplugin/netrw.lua
+++
@@ -1,5 +0,0 @@
-vim.g.netrw_preview = 1
-
-vim.api.nvim_set_hl(0, "netrwMarkFile", { reverse = true, italic = true, bold = true })
-
-vim.keymap.set("n", "q", "<cmd>enew<CR>", { buffer = 0, nowait = true })
